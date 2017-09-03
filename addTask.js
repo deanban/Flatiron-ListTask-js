5745dced1220afe90cbcf7632f64aa5fe7850b68
Gianpaul Rachiele <gianpaul817@gmail.com>
2017-09-03 14:14:44 -0400

    added find a task by list changed some variable names for readability

--- addTask.js
+++ addTask.js
@@ -4,18 +4,22 @@ const task = (function createTask() {
   let IdCounter = 0
 
   return class Task {
-    constructor(list_id, description, priority) {
-      this.list_id = list_id
+    constructor(listId, description, priority) {
+      this.listId = listId
       this.description = description
       this.priority = priority
       this.id = ++IdCounter
       all.push(this)
-      let currentList = list.find(this.list_id)[0]
+      let currentList = list.find(this.listId)[0]
       currentList.tasks.push(this)
     }
 
     static find(id) {
       return all.filter(item => item.id === parseInt(id))
+    }
+
+    static findByList(listId) {
+      return all.filter(item => item.listId === parseInt(listId))
     }
 
   }
