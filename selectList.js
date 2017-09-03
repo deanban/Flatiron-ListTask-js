0d1f21b956f4d576eb89ba73b1bea45729a24b89
deanban <pbp2005@gmail.com>
2017-09-03 13:36:27 -0400

    arrays assoc

--- selectList.js
+++ selectList.js
@@ -1,22 +1,32 @@
-const task = (function createTask() {
+// const task = (function createTask() {
 
-  let all = []
-  let IdCounter = 0
+//   let all = []
+//   let IdCounter = 0
 
+//   return class Task {
+//     constructor(list_id, description, priority) {
+//       this.list_id = list_id
+//       this.description = description
+//       this.priority = priority
+//       this.id = ++IdCounter
+//       all.push(this)
+//       let currentList = list.find(this.list_id)[0]
+//       currentList.tasks.push(this)
+//     }
+
+//     static find(id) {
+//       return all.filter(item => item.id === parseInt(id))
+//     }
+
+//   }
+// })()
+const task = function createTask(){
   return class Task {
-    constructor(list_id, description, priority) {
-      this.list_id = list_id
+    constructor(list, description, priority){
+      this.list = list
       this.description = description
       this.priority = priority
-      this.id = ++IdCounter
-      all.push(this)
-      let currentList = list.find(this.list_id)[0]
-      currentList.tasks.push(this)
+      this.list.tasks.push(this)
     }
-
-    static find(id) {
-      return all.filter(item => item.id === parseInt(id))
-    }
-
   }
-})()
+}()
